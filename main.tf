5265ccf73ba01aa0799a371725320c5d09966888
Achshah R M <91553105+Achshah-RM@users.noreply.github.com>
2023-09-24 12:32:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,6 +10,6 @@ terraform {
 
 provider "aws" {
   region     = "ap-south-1" # AWS region to deploy resources in
-  access_key = "AKIAV4YZDKPT5RHCKGRT" # AWS access key
-  secret_key = "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" # AWS secret key
+  access_key = <access_key> # AWS access key
+  secret_key = <secret_key> # AWS secret key
 }
